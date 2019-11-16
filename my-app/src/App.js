bd93c96f3d18a47f59dcbfce1c0a321355e90e1a
vumg <ab17100222@gmail.com>
2019-11-15 22:02:28 -0600

    añadi las 5 primeras categorias

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
-import Navbar from './componentes/navbar'
-import Card from './componentes/card'
+import Navbar from "./componentes/navbar";
+import Card from "./componentes/card";
 
 function App() {
   return (
     <header>
       <Navbar></Navbar>
-      <Card></Card>
+      <Card tipo={'Carne Asada'}> </Card>
+      <Card tipo={'Hamburgesas'}> </Card>
+      <Card tipo={'Posada'}> </Card>
+      <Card tipo={'Fiesta de Cumpleaños'}> </Card>
+      <Card tipo={'Fiesta de Cumpleaños'}> </Card>
     </header>
   );
 }
--- my-app/src/componentes/card.js
+++ my-app/src/componentes/card.js
@@ -1,21 +1,33 @@
 import React from "react";
-import {
-    Card, CardImg, CardBody,
-    CardTitle, 
-  } from 'reactstrap';
-import foto from './carneasada.png'
-  
-function CarneAsada() {
-  return (
-    <div>
-    <Card>
-      <CardImg src={foto} alt="Card image cap" />
-      <CardBody>
-        <CardTitle>Carne Asada</CardTitle>
-      </CardBody>
-    </Card>
-  </div>
-  );
+import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
+import carneasada from "./carneasada.png";
+
+// function CarneAsada() {
+//   return (
+//     <div>
+//       <Card>
+//         <CardImg src={carneasada} alt="Card image cap" />
+//         <CardBody>
+//           <CardTitle>Carne Asada</CardTitle>
+//         </CardBody>
+//       </Card>
+//     </div>
+//   );
+// }
+
+class CarneAsada extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card>
+          {/* <CardImg src={carneasada} alt="Card image cap" /> */}
+          <CardBody>
+            <CardTitle> {this.props.tipo}</CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default CarneAsada;
