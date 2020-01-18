05108813e0f4e1fd74eef945b874ead718ba3443
vumg <ab17100222@gmail.com>
2020-01-18 17:25:11 -0600

    responsive alert

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
-import 'typeface-roboto';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import * as serviceWorker from "./serviceWorker";
+import "typeface-roboto";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const getWidth = () => {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const messageError =
+  "Disculpa, nuestra aplicacion está hecha para que se abra desde un movil. ";
+
+if (getWidth() < 420) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+} else {
+  ReactDOM.render(
+    <div className="message">
+      <h2>{messageError}</h2>
+    </div>,
+    document.getElementById("root")
+  );
+}
+
 serviceWorker.unregister();
